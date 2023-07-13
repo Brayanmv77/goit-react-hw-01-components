94c77e3aa5951a2fd98141fce15b4dc8f5a1fafa
Brayan Medina <102181205+Brayanmv77@users.noreply.github.com>
2023-07-13 14:08:58 -0500

    TransactionsHistory component added

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import Friends from 'components/Friends/Friends';
+import TransactionHistory from 'components/Transactions/Transactions';
 
 export default function Wrapper({ data }) {
   return (
@@ -9,6 +10,7 @@ export default function Wrapper({ data }) {
       <Profile profile={data.profile}></Profile>
       <Statistics statistics={data.data} title='Upload Stats'></Statistics>
       <Friends friends={data.friends}></Friends>
+      <TransactionHistory items={data.transactions}></TransactionHistory>
     </div>
   );
 }
